3d0ed2a70bb1cc30fd40a142c7434983850eb750
Fyrolive <lilouzinzin@gmail.com>
2026-01-19 21:54:33 +0100

    First version of the Installer, need to test and change Invoke-WebRequest because downloading is to slow

--- Installer.ps1
+++ Installer.ps1
@@ -1,19 +1,24 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "Installation of youtube downloader started !"
+Write-Host "Installation of youtube downloader started"
 
-#Créating directory and path variables
-$root = Split-Path -Parent $PSScriptRoot
-$binDir = $root\yt-dlp\
-New-Item -ItemType "Directory" -Path $root -Name "yt-dlp" 
+#Creating directory and path variables
+$binDir = "$env:LOCALAPPDATA\yt-dlp"
+Write-Host "Installation path : $binDir"
+#if the file already exist, delete it
+if(Test-Path $binDir) {
+    Write-Host "Removing previous existing files..."
+    Remove-Item -Recurse -Force -Confirm:$false $binDir
+}
 
+New-Item -ItemType "Directory" -Path $binDir
 
 #yt-dlp
 Write-Host "Installation of yt-dlp started..."
 
 Invoke-WebRequest `
-    -Uri $https://github.com/yt-dlp/yt-dlp/releases/latest/yt-dlp.exe `
-    -OutFile $binDir
+    -Uri "github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe" `
+    -OutFile $binDir\yt-dlp.exe
 
 
 #ffmpeg
@@ -23,3 +28,11 @@ Invoke-WebRequest `
     -Uri "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip" `
     -OutFile "$binDir\ffmpeg.zip"
 
+Copy-Item `
+    "$binDir\ffmpeg-temp\*\bin\ffmpeg.exe" `
+    "$binDir\ffmpeg.exe"
+
+Remove-Item "$binDir\ffmpeg.zip" -Force
+Remove-Item "$binDir\ffmpeg-temp" -Recurse -Force
+
+Write-Host "Download completed !"
