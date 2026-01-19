499d177a752d135317ed8f688e34438caeece66e
Fyrolive <lilouzinzin@gmail.com>
2026-01-19 22:27:26 +0100

    Installer up and running ! Changed Invoke-WebRequest to curl.exe to speed up install process. Clean install allow the previous file to be removed and leave no zip behind.

--- Installer.ps1
+++ Installer.ps1
@@ -13,26 +13,38 @@ if(Test-Path $binDir) {
 
 New-Item -ItemType "Directory" -Path $binDir
 
+
 #yt-dlp
 Write-Host "Installation of yt-dlp started..."
 
-Invoke-WebRequest `
-    -Uri "github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe" `
-    -OutFile $binDir\yt-dlp.exe
+curl.exe -L `
+    -o "$binDir\yt-dlp.exe" `
+    "github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe"
 
 
+    
 #ffmpeg
 Write-Host "Installation of ffmpeg started..."
 
-Invoke-WebRequest `
-    -Uri "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip" `
-    -OutFile "$binDir\ffmpeg.zip"
+curl.exe -L `
+    -o "$binDir\ffmpeg.zip" `
+    "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip"
+
+Expand-Archive -Path "$binDir\ffmpeg.zip" -DestinationPath $binDir\ffmpeg-temp -Force
 
 Copy-Item `
     "$binDir\ffmpeg-temp\*\bin\ffmpeg.exe" `
     "$binDir\ffmpeg.exe"
+Copy-Item `
+    "$binDir\ffmpeg-temp\*\bin\ffplay.exe" `
+    "$binDir\ffplay.exe"
+Copy-Item `
+    "$binDir\ffmpeg-temp\*\bin\ffprobe.exe" `
+    "$binDir\ffprobe.exe"
 
 Remove-Item "$binDir\ffmpeg.zip" -Force
 Remove-Item "$binDir\ffmpeg-temp" -Recurse -Force
 
+
+#end
 Write-Host "Download completed !"
